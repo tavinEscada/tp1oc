721e2c63a95a54b2da140490597f214210a725d5
otavio tavares <otavio.sabino@ufv.br>
2026-04-10 15:25:56 -0300

    Update montador.py

--- src/montador.py
+++ src/montador.py
@@ -1,5 +1,69 @@
 import re
 
+class Comando:
+    def _innit_(self, funct7, rs2, rs1, funct3, rd, opcode, immediate, tipo):
+        self.tipo = tipo
+        self.funct7 = funct7
+        self.rs2 = rs2
+        self.rs1 = rs1
+        self.funct3 = funct3
+        self.rd = rd
+        self.opcode = opcode
+        self.immediate = immediate
+
+def indentificaComando(instrucao, c):
+    c.immediate = ''
+    c.rs2 = ''
+    c.funct7 = ''
+    c.opcode = ''
+    c.immediate = ''
+
+    match instrucao[0]:
+        case 'add' | 'beq' | 'addi' | 'lb' | 'sb' | 'sub':
+            c.funct3 = '000'
+
+        case 'lh' | 'sh' | 'sll':
+            c.funct3 = '001'
+
+        case 'lw' | 'sw':
+            c.funct3 = '010'
+
+        case 'sd':
+            c.funct3 = '011'
+
+        case 'xor':
+            c.funct3 = '100'
+
+        case 'srl':
+            c.funct3 = '101'
+
+        case 'ori' | 'or':
+            c.funct3 = '110'
+
+        case 'andi' | 'and':
+            c.funct3 = '111'
+
+    match instrucao[0]:
+        case 'add' | 'sll' | 'xor' | 'or' | 'and':
+            c.funct7 = '0000000'
+
+        case _:
+            c.funct7 = '0100000'
+
+    match instrucao[0]:
+        case 'add':
+            c.opcode = '0110011'
+
+        case 'beq' | 'bne':
+            c.opcode = '1100011'
+
+        case 'lb' | 'lh' | 'lw':
+            c.opcode = '0000011'
+
+    
+
+
+
 #ler nome do arquivo
 nomeArquivo = input("")
 
@@ -18,25 +82,32 @@ for linha in vetInstrucoes:
     instrucao = instrucao.split()
 
     #print(instrucao)
+    c = Comando()
+    
 
     #definir formato a partir da primeira palavra
     match instrucao[0]:
         case 'add' | 'and' | 'or' | 'sub' | 'sll' | 'xor' | 'srl':
             
-            #tipo R
-            print("R")
+            c.tipo = 'r'
 
+            #identifica inst
+            
 
         case 'addi' | 'andi' | 'ori' | 'li' | 'lw' | 'lh':
             
-            #I
-            print("I")
+            c.tipo = 'i'
+
 
         case 'sw' | 'sh' | 'sb':
-            #S
-            print("s")
+            
+            c.tipo = 's'
 
         case _:
-            #B
-            print("B")
+            
+            c.tipo = 'b'
+
+    indentificaComando(instrucao, c)
+    if(c.tipo == 'r'):
+        print(c.funct7 + "xxxxxx" + c.funct3 + "xxx" + c.opcode)
 
